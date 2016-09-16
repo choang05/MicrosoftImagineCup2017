4c9b9714b99190be74e1f051f6b152d434c19126
Chad Hoang <chad.hoang@yahoo.com>
2016-09-15 19:44:38 -0500

    Added minimaps to show when/where player cannot teleport due to collisions

---
+++ Assets/_Scripts/MiniMapDot.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapDot : MonoBehaviour
+{
+    public WorldType thisWorldType;
+    public enum WorldType { present, past, future };
+
+    WorldChanger worldChanger;
+    ColorAssigner colorAssigner;
+
+    void Awake()
+    {
+        //  Find and assign references
+        worldChanger = FindObjectOfType<WorldChanger>();
+        colorAssigner = GetComponent<ColorAssigner>();
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //  Present
+        if (thisWorldType == WorldType.present)
+        {
+            if (worldChanger.canSwitchPresent)
+                colorAssigner.ChangeColor(Color.green, false, true, true);
+            else
+                colorAssigner.ChangeColor(Color.red, false, true, true);
+        }
+        //  Past
+        else if (thisWorldType == WorldType.past)
+        {
+            if (worldChanger.canSwitchPast)
+                colorAssigner.ChangeColor(Color.green, false, true, true);
+            else
+                colorAssigner.ChangeColor(Color.red, false, true, true);
+        }
+        //  Future
+        else if (thisWorldType == WorldType.future)
+        {
+            if (worldChanger.canSwitchFuture)
+                colorAssigner.ChangeColor(Color.green, false, true, true);
+            else
+                colorAssigner.ChangeColor(Color.red, false, true, true);
+        }
+    }
+}
