04b42e95d0aff60172dcf54dc2e5c45589694063
Chad Hoang <chad.hoang@yahoo.com>
2016-09-29 15:55:36 -0500

    Added Cape physics. Parameters can be fined tuned. Need CapePhysicsHelper prefab to use.

---
+++ Assets/_Scripts/Player/CapePhysicsHelper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapePhysicsHelper : MonoBehaviour
+{
+    public Transform capeControlNode;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        capeControlNode.transform.position = transform.position;
+	}
+}
