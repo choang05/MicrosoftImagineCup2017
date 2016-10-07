6277e2f1c43c3888482e9ec3daf3d8cc08f60a06
sagarlimaye <sagarl3232@hotmail.com>
2016-10-07 15:09:10 -0500

    Changed background and added Play button logic

---
+++ Assets/_Scripts/UI/StartButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+  
+    public void LoadSceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }   
+}
