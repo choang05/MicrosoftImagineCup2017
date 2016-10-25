88b76f8efbcf3124eda07b2dcd685fa1096dfea1
sagarlimaye <sagarl3232@hotmail.com>
2016-10-25 16:57:52 -0500

    Added resolution menu, pause menu, game saving functionality
    The GameManager object (present throughout the game) now has functions
    to Save and Load preferences and game data.

--- Assets/_Scripts/UI/ExitButton.cs
+++ Assets/_Scripts/UI/ExitButton.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 public class ExitButton : MonoBehaviour
 {
 
-    public void Quit()
+    public void SaveAndQuit()
     {
+        GameManager.manager.SaveData();
+
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
