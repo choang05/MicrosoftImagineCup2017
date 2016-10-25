e3175cb72729d55be9084c676d5fb7a170734211
Chad Hoang <chad.hoang@yahoo.com>
2016-10-25 17:09:33 -0500

    Updated stuff

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -23,10 +23,7 @@ public class GameManager : MonoBehaviour
         else if (control != this)
             Destroy(gameObject);
         #endregion
-    }
 
-    void Start()
-    {
         SetUpPlayer();
     }
 
