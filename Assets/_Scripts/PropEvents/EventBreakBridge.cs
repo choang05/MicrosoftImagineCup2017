ce128d580fd7eb7f09059304f3dc3c8e01c82066
Chad Hoang <chad.hoang@yahoo.com>
2016-10-27 15:23:13 -0500

    Added rope editor for 2D using 3D colliders

---
+++ Assets/_Scripts/PropEvents/EventBreakBridge.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventBreakBridge : MonoBehaviour
+{
+    public HingeJoint firstHinge;
+    public HingeJoint secondHinge;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tags.Player))
+        {
+            StartCoroutine(CoBreakBridge());
+        }
+    }
+
+    private IEnumerator CoBreakBridge()
+    {
+        firstHinge.breakForce = 0;
+
+        yield return new WaitForSeconds(.25f);
+
+        secondHinge.breakForce = 0;
+
+        Destroy(gameObject);
+    }
+}
