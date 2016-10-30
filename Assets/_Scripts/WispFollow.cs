618f587f94a7c9eed9313e64f9ac8c05dee28cf8
Chad Hoang <chad.hoang@yahoo.com>
2016-10-30 05:32:31 -0500

    Added wisp follow logic

--- Assets/_Scripts/WispFollow.cs
+++ Assets/_Scripts/WispFollow.cs
@@ -5,11 +5,15 @@ public class WispFollow : MonoBehaviour
 {
     //  User-Assigned
     public Transform wisp1;
-    public float smoothTime = 0.3F;
+    public float minDistanceFromPlayer;
+    public float followTime;
+    public float wispFollowTime;
 
     //  Private
+    private Transform playerTransform;
     private SphereCollider sphereColl;
-    private Vector3 velocity = Vector3.zero;
+    private Vector3 followVelocity = Vector3.zero;
+    private Vector3 wispVelocity = Vector3.zero;
 
     void Awake()
     {
@@ -19,23 +23,36 @@ public class WispFollow : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        //StartCoroutine(CoFollow());    
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        Vector3 targetPosition = (Random.insideUnitSphere + sphereColl.transform.position) * sphereColl.radius;
-        while (Vector3.Distance(wisp1.transform.position, targetPosition) >= 0.5f)
-        {
-            //Debug.Log(targetPosition);
-            //Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
-            wisp1.transform.position = Vector3.SmoothDamp(wisp1.transform.position, targetPosition, ref velocity, smoothTime);
-        }
+        while(playerTransform == null)
+            playerTransform = FindObjectOfType<CharacterController2D>().transform;
 
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+        //wisp1.position = Vector3.zero;
+
+        //StartCoroutine(CoWispFollow());
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(playerTransform.position);
+        if (Vector2.Distance(transform.position, playerTransform.position) >= minDistanceFromPlayer)
+            transform.position = Vector3.SmoothDamp(transform.position, playerTransform.position, ref followVelocity, followTime * Time.deltaTime);
     }
 
     /*IEnumerator CoWispFollow()
     {
-    } */
+        while(true)
+        {
+            Vector3 targetPosition = (Random.insideUnitSphere * sphereColl.radius) + transform.position;
+            while (Vector3.Distance(wisp1.position, targetPosition) > 0.5f)
+            {
+                //Debug.Log(targetPosition);
+                //Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+                wisp1.transform.position = Vector3.SmoothDamp(wisp1.transform.position, targetPosition, ref wispVelocity, wispFollowTime * Time.deltaTime);
+                yield return null;
+            }
+            //yield return new WaitForSeconds(0.2f);
+        }
+    }*/
 }
