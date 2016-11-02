c484a1a21802a0ea4089b84854042099361c4e5e
Chad Hoang <chad.hoang@yahoo.com>
2016-11-02 08:07:14 -0500

    Parallax script now follows camera instead of player which is more accurate and less buggy

--- Assets/_Scripts/Parallax/ParallaxHelper.cs
+++ Assets/_Scripts/Parallax/ParallaxHelper.cs
@@ -1,48 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Com.LuisPedroFonseca.ProCamera2D;
 
 public class ParallaxHelper : MonoBehaviour
 {
-    private FreeParallax parallax;
-    private CharacterController2D charController;
-    private Vector3 lastPlayerPosition = Vector3.zero;
+    public float parallaxSpeedMult;
 
-	void Start()
+    private FreeParallax parallax;
+
+    void Awake()
     {
         parallax = GetComponent<FreeParallax>();
-
-        //  Try to find character
-        StartCoroutine(CoCachePlayer());
     }
     
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        if (charController == null)
-            return;
-
-        if (Vector3.Distance(charController.transform.position, lastPlayerPosition) >= 0.01f)
-        {
-            parallax.Speed = charController.velocity.x * -1;
-        }
-        else
-            parallax.Speed = 0;
-
-        lastPlayerPosition = charController.transform.position;
-
+        parallax.Speed = ProCamera2D.Instance.GameCamera.velocity.x * parallaxSpeedMult * -1;
     }
-
-    #region IEnumerator that checks for a indicator panel that may not have been created yet thus we need to keep checking till it exist.
-    IEnumerator CoCachePlayer()
-    {
-        charController = FindObjectOfType<CharacterController2D>();
-
-        //  If the charController hasn't been found yet because the character hasn't spawned yet, keep trying to find.
-        while (charController == null)
-        {
-            charController = FindObjectOfType<CharacterController2D>();
-            yield return null;
-        }
-    }
-    #endregion
 }
