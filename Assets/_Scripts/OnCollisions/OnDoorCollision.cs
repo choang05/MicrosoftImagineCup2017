a452511235e31e442435ab50c1cb22719362f1f2
Jonathan Davis <jonathanedavis@outlook.com>
2016-11-06 01:47:39 -0500

    finished update
    -added door audio
    -added footstep audio for stone and water
    -added sound asset for landing in puddle
    -changed present ambient sound

--- Assets/_Scripts/OnCollisions/OnDoorCollision.cs
+++ Assets/_Scripts/OnCollisions/OnDoorCollision.cs
@@ -4,9 +4,23 @@ using System.Collections;
 public class OnDoorCollision : OnPlayerCollision
 {
     public float ForceMultiplier;
+    private AudioSource doorSound;
+
+    void Awake()
+    {
+        doorSound = GetComponent<AudioSource>();
+    }
 
     public override void OnPlayerHit(ControllerColliderHit hit, Vector3 pushDir)
     {
         hit.rigidbody.velocity = pushDir * ForceMultiplier;
+
+        // door creak audio
+        if (!doorSound.isPlaying)
+        {
+            playerAudio.randomizePitch(doorSound);
+            doorSound.volume *= playerAudio.randomVolume();
+            doorSound.Play();
+        }
     }
 }
