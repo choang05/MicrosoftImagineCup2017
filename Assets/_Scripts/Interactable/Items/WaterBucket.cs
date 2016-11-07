1ff6a1f4b994c3ff5de2dac6c43927f51dd33b14
Jonathan Davis <jonathanedavis@outlook.com>
2016-11-06 23:11:05 -0600

    Started on water bucket
    might revert or change

--- Assets/_Scripts/Interactable/Items/WaterBucket.cs
+++ Assets/_Scripts/Interactable/Items/WaterBucket.cs
@@ -5,12 +5,24 @@ public class WaterBucket : MonoBehaviour
 {
     public GameObject unfilledSprite;
     public GameObject filledSprite;
-    public bool hasWater; 
+    public bool hasWater;
+
+    public AudioClip fillwithwater;
+    public AudioClip pourwater;
+    private AudioSource bucketSound;
+
+    void Awake()
+    {
+        bucketSound = GetComponent<AudioSource>();
+    }
 
     public void SetBucketWater(bool isFilled)
     {
         if (isFilled)
         {
+            playerAudio.randomizePitch(bucketSound);
+            bucketSound.PlayOneShot(fillwithwater, playerAudio.randomVolume());
+
             hasWater = true;
 
             unfilledSprite.SetActive(false);
@@ -22,6 +34,9 @@ public class WaterBucket : MonoBehaviour
 
             unfilledSprite.SetActive(true);
             filledSprite.SetActive(false);
+
+            playerAudio.randomizePitch(bucketSound);
+            bucketSound.PlayOneShot(pourwater, playerAudio.randomVolume());
         }
     }
 }
