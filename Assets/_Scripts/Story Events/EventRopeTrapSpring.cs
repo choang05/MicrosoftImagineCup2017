f4d5e48606040786ac08590e3dfd3ae4457f1cb9
Chad Hoang <chad.hoang@yahoo.com>
2016-11-07 01:46:06 -0600

    Started Phase 2 puzzle. Need to figure out spring trap logic.

---
+++ Assets/_Scripts/Story Events/EventRopeTrapSpring.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventRopeTrapSpring : MonoBehaviour
+{
+    public HingeJoint mainRope;
+    public Rope bottomRope;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tags.Player))
+        {
+            StartCoroutine(CoSpringTrap());
+        }
+    }
+
+    IEnumerator CoSpringTrap()
+    {
+
+        mainRope.connectedAnchor = new Vector3(mainRope.connectedAnchor.x, 23, mainRope.connectedAnchor.z);
+
+        yield return null;
+
+        bottomRope.HangFirstSegment = false;
+        bottomRope.HangLastSegment = false;
+
+        //yield return new WaitForSeconds(2);
+
+        //Destroy(this);
+    }
+}
