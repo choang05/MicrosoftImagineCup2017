8779ebb8ed8035f19725327e0fed27c75d6dc927
sagarlimaye <sagarl3232@hotmail.com>
2016-12-01 16:35:56 -0600

    Added UI for interactables

---
+++ Assets/InteractableUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableUI : MonoBehaviour {
+
+    public GameObject canvasGO;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
