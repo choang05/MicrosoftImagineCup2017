05fa943847fafa9e332954c61ee807c0af01330b
Chad Hoang <chad.hoang@yahoo.com>
2017-01-19 01:19:13 -0600

    Misc.

---
+++ Assets/ChangesColorFromLever.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ChangesColorFromLever : MonoBehaviour
+{
+    //  User Defined Variables
+    public int[] AcceptedLeverIDs;
+
+    private List<int> remainingLeverIDs = new List<int>();
+    private ColorAssigner colorAssigner;
+
+    private bool isSatisfied;
+
+    //   Event Listener
+    void OnEnable()
+    {
+        Lever.OnLeverTurnLeft += OnLeverTurnLeft;
+        Lever.OnLeverTurnRight += OnLeverTurnRight;
+    }
+
+    void OnDisable()
+    {
+        Lever.OnLeverTurnLeft -= OnLeverTurnLeft;
+        Lever.OnLeverTurnRight -= OnLeverTurnRight;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        colorAssigner = GetComponent<ColorAssigner>();
+
+        remainingLeverIDs = AcceptedLeverIDs.ToList();
+    }
+
+    void OnLeverTurnLeft(int leverID)
+    {
+        if (!DoesAcceptLeverID(leverID))
+            return;
+
+        if (!remainingLeverIDs.Contains(leverID))
+        {
+            remainingLeverIDs.Add(leverID);
+
+            if (isSatisfied == true)
+            {
+                Debug.Log("Bridge is lowered, but raised again");
+            }
+
+            isSatisfied = false;
+
+            DisplayRemainingIDs();
+
+        }
+
+    }
+
+    void OnLeverTurnRight(int leverID)
+    {
+        if (!DoesAcceptLeverID(leverID))
+            return;
+
+        if (remainingLeverIDs.Contains(leverID))
+        {
+            remainingLeverIDs.Remove(leverID);
+
+            DisplayRemainingIDs();
+        }
+
+        if (remainingLeverIDs.Count <= 0)
+        {
+            isSatisfied = true;
+            Debug.Log("Bridge dropped");
+        }
+
+    }
+
+    bool DoesAcceptLeverID(int leverID)
+    {
+        if (AcceptedLeverIDs.Contains(leverID))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void DisplayRemainingIDs()
+    {
+        for (int i = 0; i < remainingLeverIDs.Count; i++)
+            Debug.Log(remainingLeverIDs[i]);
+    }
+}
--- Assets/_Scripts/Interactable/Lever.cs
+++ Assets/_Scripts/Interactable/Lever.cs
@@ -2,10 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lever : MonoBehaviour {
+public class Lever : MonoBehaviour
+{
+    public int LeverID;
+    public bool isOneShot;
 
-    public GameObject leverObject;
-    
-    
-	
+    public LeverState currentLeverState;
+    public enum LeverState
+    {
+        Left,
+        Right
+    }
+
+    public delegate void LeverTurnHandler(int leverID);
+    public static event LeverTurnHandler OnLeverTurnRight;
+    public static event LeverTurnHandler OnLeverTurnLeft;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButtonDown("Interact") && other.CompareTag(Tags.Player))
+        {
+            // Lever animation
+            if (currentLeverState == LeverState.Left && isOneShot)
+            {
+                if (OnLeverTurnRight != null)
+                    OnLeverTurnRight(LeverID);
+
+                Debug.Log("Lever " + LeverID + " turned Right and broke!");
+
+                Destroy(this);
+            }
+            else if (currentLeverState == LeverState.Left)
+            {
+                if (OnLeverTurnRight != null)
+                    OnLeverTurnRight(LeverID);
+
+                currentLeverState = LeverState.Right;
+
+                Debug.Log("Lever " + LeverID + " turned Right!");
+            }
+            else
+            {
+                if (OnLeverTurnLeft != null)
+                    OnLeverTurnLeft(LeverID);
+
+                currentLeverState = LeverState.Left;
+
+                Debug.Log("Lever " + LeverID + " turned Left!");
+            }
+        }
+    }
+
+
+
 }
